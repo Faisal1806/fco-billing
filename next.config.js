88bf67ce69f0791634f384980e3f74c4340f99ce
Faisal Firdous (Faisu) <lone07936@gmail.com>
2025-09-09 04:48:38 +0000

    Again showing this

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   serverActions: {
     bodySizeLimit: '4mb', // Allow larger image uploads for OCR
   },
