25ad58fb0590b2f974e0aa3b79f665c9860fda42
Faisal Firdous (Faisu) <lone07936@gmail.com>
2025-09-19 11:09:00 +0000

    Here is this icon simple it should be logo of our company that apple mak

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,18 +2,34 @@
 import * as React from 'react';
 
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
-    data-ai-logo
-  >
-    <path d="M12 12m-10 0a10 10 0 1 0 20 0a10 10 0 1 0 -20 0"></path>
-    <path d="M14 12a2 2 0 1 0 -4 0v-2a2 2 0 1 0 4 0"></path>
-  </svg>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        {...props}
+        data-ai-logo
+    >
+        {/* Apple body */}
+        <path
+            d="M17.4,13.26a4.42,4.42,0,0,1-1.57,3.31,4.48,4.48,0,0,1-3.83,1.43,3.41,3.41,0,0,1-1.39-.3,4,4,0,0,1-1.35-1,3.73,3.73,0,0,1-.8-1.51,5,5,0,0,1-.21-1.89A4.58,4.58,0,0,1,12.33,9a4.2,4.2,0,0,1,3.42.74,2.59,2.59,0,0,0,1.65.61,2.83,2.83,0,0,0,1.83-.71,0.22,0.22,0,0,1,.33.3,6.33,6.33,0,0,1-2.16,3.32Z"
+            fill="currentColor"
+        />
+        {/* Apple leaf */}
+        <path
+            d="M13.23,6.33a2.38,2.38,0,0,1,1.55-2.28,2.53,2.53,0,0,0-2.09,1,2.42,2.42,0,0,1-1.55,2.28,2.53,2.53,0,0,0,2.09-1Z"
+            fill="currentColor"
+        />
+        {/* Text inside apple */}
+        <text
+            x="11.5"
+            y="14.5"
+            fontFamily="sans-serif"
+            fontSize="3.5"
+            fontWeight="bold"
+            textAnchor="middle"
+            fill="var(--background, #fff)"
+        >
+            F.Co
+        </text>
+    </svg>
 );
