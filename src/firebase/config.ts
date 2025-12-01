9d24bf2bce7dd6c14a110bb4683b0fd0532198b5
Faisal Firdous (Faisu) <lone07936@gmail.com>
2025-12-01 13:56:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "swiftsale-ewd7o",
+  "appId": "1:693507183352:web:62935a0e75eb4a261e610d",
+  "apiKey": "AIzaSyDreV3-idwKsbpZI8UL9MT_GHssbLaEUo4",
+  "authDomain": "swiftsale-ewd7o.firebaseapp.com",
+  "measurementId": "G-F7TTL0TYRC",
+  "messagingSenderId": "693507183352"
+};
