28d914c18077b23c8162e6a750db327346ae3ed5
Faisal <lone07936@gmail.com>
2026-05-20 14:42:52 +0530

    fixed restore api mongodb connection

--- src/app/api/restore-json/route.js
+++ src/app/api/restore-json/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import connectDB from "@/lib/db";
+import { connectDB } from '@/lib/db';
 import Customer from "../../../models/customer";
 
 export async function POST(req) {
